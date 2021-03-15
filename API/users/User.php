2310f06f48cec46a7951752abb3c464b0a60ede3
AndrewAndrewsen <mail@andersandersson.net>
2021-03-15 14:28:40 +0100

    Add getUser

--- API/users/User.php
+++ API/users/User.php
@@ -8,6 +8,7 @@ class User {
     private $username;
     private $user_email;
     private $user_token;
+    private $password;
     
 
     function __construct($db) {
@@ -53,16 +54,44 @@ class User {
 
         
        } else {
-            echo "All arguments need a value!";
+            $error = new stdClass();
+            $error->message = "All arguments need a value!";
+            $error->code = "0001";
+            print_r(json_encode($error));
             die();
        }
 
     }
 
-
     function GetAllUsers() {
-            
+        $sql = "SELECT username, email, password FROM users";
+        $statement = $this->database_connection->prepare($sql);
+        $statement->execute();
+        echo json_encode($statement->fetchAll());
+    }
+
+    function GetUser($user_id) {
+        $sql = "SELECT id, username, email, password FROM users WHERE id=:user_id_IN";
+        $statement = $this->database_connection->prepare($sql);
+        $statement->bindParam(":user_id_IN", $user_id);
         
+        if( !$statement->execute() || $statement->rowCount() < 1 ) {
+            $error = new stdClass();
+            $error->message = "User does not exist!";
+            $error->code = "0003";
+            print_r(json_encode($error));
+            die();
+        }
+
+        $row = $statement->fetch();
+
+        $this->username = $row['username'];
+        $this->user_email = $row['email'];
+        $this->password = $row['password'];
+        $this->user_id = $row['id'];
+        
+        print_r(json_encode($row));
+
     }
 
 
