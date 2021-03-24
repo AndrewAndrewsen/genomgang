07030ec19acced3d605ae52cac4ba2489b144110
AndrewAndrewsen <mail@andersandersson.net>
2021-03-24 09:54:33 +0100

    Add login, session & tokens

--- API/index.php
+++ API/index.php
@@ -1,3 +1,14 @@
 <h1>Blogg api!</h1>
 
 <h2>Endpoints:</h2>
+
+
+$arr[0];
+$arr[1];
+$arr[2];
+$arr[3];
+$arr[4];
+
+for($i = 0; $i < count($arr); $i++) {
+    echo $arr[$i];
+}
--- API/users/getAll.php
+++ API/users/getAll.php
@@ -2,9 +2,26 @@
     include("../db.php");
     include("User.php");
 
+    $token = "";
+    if(isset($_GET['token'])) {
+        $token = $_GET['token'];
+    } else {
+        $error = new stdClass();
+        $error->message = "No token specified!";
+        $error->code = "0009";
+        print_r(json_encode($error));
+        die();
+    }
+
     $user = new User($pdo);
-    $users = $user->GetAllUsers();
-    print_r(json_encode($users));
 
-
-?>
+    if($user->isTokenValid($token)) {
+        $users = $user->GetAllUsers();
+        print_r(json_encode($users));
+    
+    } else {
+        $error = new stdClass();
+        $error->message = "Invalid token! Login to create a new token.";
+        $error->code = "0010";
+        print_r(json_encode($error));
+    }
